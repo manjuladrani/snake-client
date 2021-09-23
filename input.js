7ff25f4e5e6dcd7d3334bf18c21da015fb3f65b9
manjuladrani <88101950+manjuladrani@users.noreply.github.com>
2021-09-22 22:46:09 -0400

    Update input.js

--- input.js
+++ input.js
@@ -3,25 +3,27 @@ let connection;
 const setupInput = function (conn) {
   connection = conn;
   const stdin = process.stdin;
+  stdin.on("data", handleUserInput);
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
   return stdin;
 };
-let letter;
 
 const handleUserInput = function (key) {
 
   // your code here
-
+  if (key === '\u0003') {
+    process.exit();
+  }
   if (key === w) {
-     letter.write("Move: Up");
+     connection.write("Move: Up");
   } else if (key === a) {
-      letter.write("Move: left");
+      connection.write("Move: left");
   } else if (key === s) {
-      letter.write("Move: down");
+      connection.write("Move: down");
   } else if (key === d) {
-      letter.write("Move: righr");
+      connection.write("Move: righr");
   } else {
       letter.write("Sat: Whoooooo");
   }
