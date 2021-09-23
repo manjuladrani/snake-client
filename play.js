3319918fcbf5cc3f0a87cf0eb7602edeebdcf4b2
manjuladrani <88101950+manjuladrani@users.noreply.github.com>
2021-09-22 21:20:10 -0400

    Update play.js

--- play.js
+++ play.js
@@ -1,20 +1,6 @@
-const net = require("net");
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '135.23.223.133',
-    port: 50542
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  return conn;
-};
-conn.on("connect", () => {
-    console.log("you ded cuz you idled");
-    // code that does something when the connection is first established
-  });
 console.log("Connecting ...");
 connect();
+setupInput();
